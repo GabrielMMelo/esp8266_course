6a40a91749a89badb9721a0fcee6a2eecdf6a2d1
GabrielMMelo <gabrielmelocomp@gmail.com>
2018-10-16 20:15:07 +0000

    Http example updated

--- examples/micropython/http.py
+++ examples/micropython/http.py
@@ -13,3 +13,35 @@ def http_get(url):
             else:
                 break
                 s.close()
+
+def http_server():
+    html = """<!DOCTYPE html>
+    <html>
+        <head> <title>ESP8266 Pins</title> </head>
+        <body> <h1>ESP8266 Pins</h1>
+            <table border="1"> <tr><th>Pin</th><th>Value</th></tr> %s </table>
+        </body>
+    </html>
+    """
+    addr = socket.getaddrinfo('192.168.0.101', 80)[0][-1]
+
+    s = socket.socket()
+    s.bind(addr)
+    s.listen(1)
+
+    print('listening on', addr)
+
+    while True:
+            cl, addr = s.accept()
+            print('client connected from', addr)
+            cl_file = cl.makefile('rwb', 0)
+            while True:
+                    line = cl_file.readline()
+                    if not line or line == b'\r\n':
+                            break
+#            rows = ['<tr><td>%s</td><td>%d</td></tr>' % (str(p), p.value()) for p in pins]
+#            response = html % '\n'.join(rows)
+            test = "teste" 
+            response = html % '\n'.join(test)
+            cl.send(response)
+            cl.close()
